aae49869ad3ece424e0a36aafa3474d6a1b72789
xe52360 <fernando.santa.contractor@bbva.com>
2016-10-04 17:33:12 +0200

    Initial commit

---
+++ web/main.dart
@@ -0,0 +1,10 @@
+import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
+import 'package:polymer/polymer.dart';
+import 'package:angular2/platform/browser.dart';
+
+import 'package:tic_tac_toe/views/main_app/main_app.dart';
+
+main() async {
+  await initPolymer();
+  bootstrap(MainApp);
+}
